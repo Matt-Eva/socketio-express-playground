9cb2a3e8f701a377bc75ec5ba9723e7e76a63d42
Matt Eva <matteweva@gmail.com>
2023-07-06 21:02:20 -0700

    added session logic to socket-io

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 const express = require('express');
+const session = require('express-session');
 const http = require('http');
 const socketIO = require('socket.io');
 const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
-
-app.use(cors())
+const sessionMiddleware = session({
+    secret: "test"
+})
 
 const io = socketIO(server, {
     cors: {
         origin: "http://127.0.0.1:5500"
     }
-});
+}); 
+
+app.use(cors())
+
+app.use(sessionMiddleware)
+
+io.engine.use(sessionMiddleware)
 
 io.on("connection", (socket) =>{
+    console.log("socket", socket.request.session)
     console.log("a user connected")
     socket.on("message", (arg) =>{
         console.log(arg)
         socket.emit("broadcast", arg)
     })
+})
+
+app.get("/", (req, res) =>{
+    // console.log("FETCH", req)
+    console.log("SESSION", req.session)
+    res.send({data: "data"})
 })
 
 server.listen(4000, () => {
